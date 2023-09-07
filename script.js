8eb68f06e016d9259a6d825ac166319d5ec05077
Jessie Heston <138740614+jessieheston@users.noreply.github.com>
2023-09-07 19:21:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,88 @@
-const canvas = document.getElementById('canvas1')
+let playerState = 'idle';
+const dropdown = document.getElementById('animations')
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+});
+
+const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png'
+const spritewidth = 575;
+const spriteHeight = 523;
+
+
+let gameFrame = 0;
+const staggerFrames = 5;
+const spriteAnimations = []; 
+const animateStates = [
+    {
+      name: 'idle',
+      frames: 7,
+    },
+    {
+      name: 'jump',
+      frames: 7,
+    },
+    {
+      name: 'fall',
+      frames: 7,
+    },
+    {
+      name: 'run',
+      frames: 9,
+    },
+    {
+      name: 'dizzy',
+      frames: 11,
+    },
+    {
+      name: 'sit',
+      frames: 5,
+    },
+    {
+      name: 'roll',
+      frames: 7,
+    },
+    {
+      name: 'bite',
+      frames: 7,
+    },
+    {
+      name: 'ko',
+      frames: 12,
+    },
+    {
+      name: 'gethit',
+      frames: 4,
+    }
+];
+animateStates.forEach((state, index) => {
+   let frames = {
+       loc: []
+   }
+   for (let j = 0; j < state.frames; j++){
+        let positionX = j * spritewidth; 
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+   }
+   spriteAnimations[state.name] = frames;
+});
+
+function animate(){
+   ctx.clearRect (0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+   let position = Math. floor (gameFrame/staggerFrames) % spriteAnimations[playerState]
+   .loc.length;
+   let frameX = spritewidth * position;
+   let frameY = spriteAnimations[playerState].loc[position].y;
+
+   ctx.drawImage(playerImage, frameX, frameY, spritewidth,
+   spriteHeight, 0, 0, spritewidth, spriteHeight);
+
+   gameFrame++;
+   requestAnimationFrame(animate);
+   };
+animate();

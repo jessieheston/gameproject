4fe60415a3241a46dcff46c0bb999102b7ab0be9
Jessie Heston <jessie@Jessies-Air.lan>
2023-09-08 14:14:16 -0400

    class Enemy

--- project3/script.js
+++ project3/script.js
@@ -3,6 +3,8 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 900;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 class Enemy {
     constructor(){
@@ -19,12 +21,18 @@ class Enemy {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 };
-const enemy1 = new Enemy();
 
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
+console.log(enemiesArray);
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update(); 
-    enemy1.draw();
+    //enemy1.update(); 
+    //enemy1.draw();
+enemiesArray.forEach(enemy => {
+
+});
     requestAnimationFrame(animate);
 }
 animate();
